03db15b303cd7e19cf2e8d83faa7c108518f78e1
Den <example@mail.com>
2025-10-02 08:27:31 +0700

    charinfo&marvelservices

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -5,34 +5,48 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
 import MarvelService from '../../services/MarvelService';
 
 class CharInfo extends Component {
 
   state = {
-    char: {}, // объект, который будет хранить информацию о персонаже
+    char: null , // объект, который будет хранить информацию о персонаже
     loading: false, // правая часть макета не должна загружаться сразу и загрузка будет только по действию пользователя
     error: false // индикатор ошибки
   }
 
   marvelService = new MarvelService(); // создаем экземпляр класса MarvelService для получения данных о персонаже
 
-  // Это нужно для того, чтобы загрузить информацию о персонаже, если его id уже передан через prop в app
+//* ComponentDidMount вызывается один раз сразу после того, как компонент добавлен в DOM (т.е. после первого рендера).
+
+// Монтирование — это процесс создания компонента и добавления его в реальный DOM, включая инициализацию state, вызов render и отображение результата.
+// Виртуальный DOM — это абстракция реального DOM, которая позволяет React эффективно обновлять интерфейс, минимизируя изменения в реальном DOM.
+// При монтировании React создаёт виртуальный DOM на основе метода render и преобразует его в реальный DOM. Сравнение виртуального и реального DOM происходит при обновлениях компонента, чтобы вносить только необходимые изменения.
+// После монтирования компонента componentDidMount вызывается автоматически, позволяя выполнять действия, такие как запросы к API или установка таймеров.
   componentDidMount() {
     this.updateChar();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // prevProps - предыдущие props (свойства), которые были до обновления компонента
+    // prevState - предыдущий state, который был до обновления компонента
+    if (this.props.charId !== prevProps.charId) { // если текущий charId не равен предыдущему charId, значит произошло изменение props
+      this.updateChar();
+    }
   }
 
   // updateChar - метод, который будет обновлять информацию о персонаже
   // вызывается при изменении props (при клике на персонажа в CharList)
   updateChar = () => {
     const {charId} = this.props; // получаем id персонажа из props (передается из App.js)
+    // this.props - объект, который хранит свойства компонента (передаются из родительского компонента)
     if (!charId) {
-      return; // если id нет, выходим из функции, чтобы не делать запрос на сервер, если id нет
+      return; // если id нет, выходим из функции, чтобы не делать запрос на сервер
     }
 
     this.oncharLoading(); // показываем спиннер, пока идет загрузка
 
+    // this.marvelService - экземпляр класса MarvelService
     this.marvelService
       .getCharacter(charId) // вызываем метод getCharacter из MarvelService, передавая id персонажа
       .then(this.onCharLoaded) // когда промис resolve, вызываем метод onCharLoaded
@@ -41,6 +55,7 @@ class CharInfo extends Component {
 
   // функция, которая обновляет state компонента
   onCharLoaded = (char) => { // метод класса, который обновляет state компонента
+    // setstate - метод, который обновляет state компонента и вызывает перерисовку компонента
     this.setState({char, loading: false}) // обновляем state компонента CharInfo
   // * Как только загружаются данные, позиция loading меняется на false и спиннер исчезает
   // в char приходит объект с данными персонажа
@@ -71,70 +86,63 @@ class CharInfo extends Component {
     const errorMessage = error ? <ErrorMessage/> : null; // если error true, то показываем компонент ErrorMessage, иначе null
     //? 6
     const spinner = loading ? <Spinner/> : null; // если loading true, то показываем компонент Spinner, иначе null
-    const content = !(loading || error) ? <View char={char}/> : null; // если loading и error false, то показываем компонент View, иначе null
-
+    const content = !(loading || error || !char) ? <View char={char}/> : null; // если нет загрузки, нет ошибки и есть данные о персонаже, то показываем компонент View, иначе null
 
     return (
       <div className="char__info">
-
+        {/*
+        Если уже есть данные о персонаже (char), или идёт загрузка (loading), или произошла ошибка (error), то skeleton будет null (ничего не показывается).
+        Если данных нет, загрузки нет и ошибки нет — показывается компонент <Skeleton/>, который служит "пустым" шаблоном до выбора персонажа.
+        */}
+        {skeleton}
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     )
   }
 }
 
 const View = ({char}) => {
+  const {name, description, thumbnail, homepage, wiki, comics} = char; // деструктуризация объекта char
+  // от куда берется char? - из props, который передается в CharInfo из App.js
+  // данные попадют через метод onCharLoaded, который обновляет state компонента CharInfo
   return (
     // компонент должен возвращать один корневой элемент. Если убрать <>...</>, а оставить несколько элементов на одном уровне, React выдаст ошибку
     <>
       <div className="char__basics">
-        <img src={thor} alt="abyss"/>
+        <img src={thumbnail} alt={name}/>
         <div>
-          <div className="char__info-name">thor</div>
+          <div className="char__info-name">{name}</div>
           <div className="char__btns">
-            <a href="#" className="button button__main">
+            <a href={homepage} className="button button__main">
                 <div className="inner">homepage</div>
             </a>
-            <a href="#" className="button button__secondary">
+            <a href={wiki} className="button button__secondary">
                 <div className="inner">Wiki</div>
             </a>
           </div>
         </div>
       </div>
       <div className="char__descr">
-        In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.
+        {description}
       </div>
       <div className="char__comics">Comics:</div>
       <ul className="char__comics-list">
-        <li className="char__comics-item">
-          All-Winners Squad: Band of Heroes (2011) #3
-        </li>
-        <li className="char__comics-item">
-          Alpha Flight (1983) #50
-        </li>
-        <li className="char__comics-item">
-          Amazing Spider-Man (1999) #503
-        </li>
-        <li className="char__comics-item">
-          Amazing Spider-Man (1999) #504
-        </li>
-        <li className="char__comics-item">
-          AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-        </li>
-        <li className="char__comics-item">
-          Vengeance (2011) #4
-        </li>
-        <li className="char__comics-item">
-          Avengers (1963) #1
-        </li>
-        <li className="char__comics-item">
-          Avengers (1996) #1
-        </li>
+        {comics.length > 0 ? null : 'There is no comics with this character'} {/* если нет комиксов, то показываем сообщение */}
+        {
+          comics.map((item, i) => {
+            // eslint-disable-next-line
+            if (i > 9) return; // если i больше 10, то выходим из функции, чтобы не показывать больше 10 комиксов
+            return (
+              // i берется из массива comics, который приходит из API
+              <li key={i} className="char__comics-item">
+                {/* item - это элемент массива comics, который приходит из API */}
+                {item}
+              </li>
+            )
+          })
+        }
       </ul>
     </>
   )
