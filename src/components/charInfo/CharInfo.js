24c7c16fe18925b8102d97ebc02defbbfc2433b2
Den <example@mail.com>
2025-10-05 10:16:32 +0700

    PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types';
+
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -147,6 +149,11 @@ const View = ({char}) => {
       </ul>
     </>
   )
+}
+
+CharInfo.propTypes = {
+  charId: PropTypes.number // charId должен быть числом
+  // PropTypes - это библиотека для проверки типов props
 }
 
 export default CharInfo;
